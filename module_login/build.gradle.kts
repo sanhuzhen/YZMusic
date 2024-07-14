fcc275d30be1560382f7a522f71cc74d5b7b94fa
test <1987901354@qq.com>
2024-07-14 23:43:28 +0800

    解决了单模块无法运行的问题

--- module_login/build.gradle.kts
+++ module_login/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.module.login"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
