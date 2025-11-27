2f5f424a658edfc501d1d81ad91a214d92cee547
you <2719757561@qq.com>
2025-11-27 21:17:41 +0800

    :sparkles: 自动添加所有模块功能实现

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,18 +23,13 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "YZMusic"
-include(":app")
-include(":lib_base")
-include(":lib_net")
-include(":module_login")
-include(":module_recommend")
-include(":module_mine")
-include(":module_hot")
-include(":module_songlist")
-include(":module_search")
-include(":module_mvplayer")
-include(":module_songlist")
-include(":module_musicplayer")
+// 自动包含所有模块
+rootDir
+    .listFiles { file -> file.isDirectory && (File(file, "build.gradle.kts").exists() || File(file, "build.gradle").exists()) }
+    ?.forEach { dir ->
+        include(":${dir.name}")
+        project(":${dir.name}").projectDir = dir
+    }
 
 // 如果 build 窗口乱码，去 顶部栏 - Help - Edit Custom VM Options 里面添加 -Dfile.encoding=UTF-8，然后重启 AS
 // 制作网址：http://patorjk.com/software/taag/
@@ -48,3 +43,5 @@ val redrock = """
    ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓██████▓▒░  
 """.trimIndent()
 println(redrock)
+
+// 新增模块会出现在下面include(":xx")，请删除，上面会自动增加
